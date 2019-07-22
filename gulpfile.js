efefa396a15469cf5a790e45f8ad485c7bcf5f3f
Ale Mohamad <hello@alemohamad.com>
2019-07-22 15:08:14 +0200

    Updated gulp script to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var csso         = require('gulp-csso');
 var del          = require('del');
 var gulp         = require('gulp');
 var htmlmin      = require('gulp-htmlmin');
-var runSequence  = require('run-sequence');
 var sass         = require('gulp-sass');
 var uglify       = require('gulp-uglify');
 var path         = require('path');
@@ -14,31 +13,39 @@ var bs           = require('browser-sync').create();
 var bourbon      = require('bourbon').includePaths;
 var breakpoint   = 'node_modules/breakpoint-sass/stylesheets';
 
-// Set the browser that you want to support
-const AUTOPREFIXER_BROWSERS = [
-  'ie >= 10',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
-];
+var paths = {
+  styles: {
+    src: 'src/sass/**/*.scss',
+    dest: './dist/css'
+  },
+  scripts: {
+    src: 'src/js/**/*.js',
+    dest: './dist/js'
+  },
+  html: {
+    src: 'src/**/*.html',
+    dest: './dist'
+  },
+  assets: {
+    src: './src/images/**/*.*',
+    dest: './dist/images'
+  }
+};
 
 // Gulp task to configure the browser sync instance
-gulp.task('browser-sync', function() {
+function browser_sync(done) {
   bs.init({
-      server: {
-          baseDir: "./dist/"
-      }
+    server: {
+      baseDir: paths.html.dest
+    }
   });
-});
+
+  done();
+}
 
 // Gulp task to minify CSS files
-gulp.task('styles', function () {
-  return gulp.src('./src/sass/styles.scss')
+function css(done) {
+  gulp.src('./src/sass/styles.scss')
     // Compile SASS files
     .pipe(sass({
       outputStyle: 'nested',
@@ -47,61 +54,87 @@ gulp.task('styles', function () {
       onError: console.error.bind(console, 'Sass error:')
     }))
     // Auto-prefix css styles for cross browser compatibility
-    .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    .pipe(autoprefixer())
     // Minify the file
     .pipe(csso())
     // Output
-    .pipe(gulp.dest('./dist/css'))
+    .pipe(gulp.dest(paths.styles.dest))
     .pipe(bs.reload({stream: true}))
-    .pipe(notify({ title: 'Web Dev Kit', message: 'Styles task complete', icon: path.join(__dirname, 'applogo.png') }))
-});
+    .pipe(notify({
+      "title": "Web Dev Kit",
+      "message": "Styles task complete",
+      "icon": path.join(__dirname, "applogo.png")
+    }));
+
+  done();
+}
 
 // Gulp task to minify JavaScript files
-gulp.task('scripts', function() {
-  return gulp.src('./src/js/**/*.js')
+function js(done) {
+  gulp.src('./src/js/**/*.js')
     // Minify the file
     .pipe(uglify())
     // Output
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(gulp.dest(paths.scripts.dest))
     .pipe(bs.reload({stream: true}))
-    .pipe(notify({ title: 'Web Dev Kit', message: 'Scripts task complete', icon: path.join(__dirname, 'applogo.png') }))
-});
+    .pipe(notify({
+      "title": "Web Dev Kit",
+      "message": "Scripts task complete",
+      "icon": path.join(__dirname, "applogo.png")
+    }));
+
+  done();
+}
 
 // Gulp task to minify HTML files
-gulp.task('html', function() {
-  return gulp.src(['./src/**/*.html'])
+function html(done) {
+  gulp.src('./src/**/*.html')
     .pipe(htmlmin({
       collapseWhitespace: true,
       removeComments: true
     }))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(paths.html.dest))
     .pipe(bs.reload({stream: true}))
-    .pipe(notify({ title: 'Web Dev Kit', message: 'HTML task complete', icon: path.join(__dirname, 'applogo.png') }))
-});
+    .pipe(notify({
+      "title": "Web Dev Kit",
+      "message": "HTML task complete",
+      "icon": path.join(__dirname, "applogo.png")
+    }));
+
+  done();
+}
 
 // Gulp task to copy other assets
-gulp.task('assets', function() {
-  return gulp.src('./src/images/**/*.*')
+function assets(done) {
+  gulp.src(paths.assets.src)
     // Output
-    .pipe(gulp.dest('./dist/images'))
-});
+    .pipe(gulp.dest(paths.assets.dest));
+
+  done();
+}
 
 // Clean output directory
-gulp.task('clean', () => del(['dist']));
+function clean(done) {
+  del(['dist']);
 
-// Gulp task to minify all files
-gulp.task('default', ['clean', 'browser-sync'], function () {
-  runSequence(
-    'styles',
-    'scripts',
-    'html',
-    'assets'
-  );
-});
+  done();
+}
 
-// Gulp task to watch changes
-gulp.task('watch', ['default'], function() {
-  gulp.watch('src/sass/**/*.scss', ['styles']);
-  gulp.watch('src/js/**/*.js', ['scripts']);
-  gulp.watch('src/**/*.html', ['html']);
-});
+gulp.task('clean', clean);
+gulp.task('css', css);
+gulp.task('js', js);
+gulp.task('html', html);
+gulp.task('assets', assets);
+
+gulp.task('default', gulp.parallel(css, js, html, assets));
+
+function watch_files(done) {
+  gulp.watch(paths.styles.src, css);
+  gulp.watch(paths.scripts.src, js);
+  gulp.watch(paths.html.src, html);
+  gulp.watch(paths.assets.src, assets);
+
+  done();
+}
+
+gulp.task('watch', gulp.parallel('default', watch_files, browser_sync));
